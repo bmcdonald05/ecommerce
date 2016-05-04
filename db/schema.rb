5287a4b1b7bdfc99cb7806c9a9f3b5ffcd9afdcd
Brian McDonald <bmcdonald05@gmail.com>
2016-05-04 08:49:12 -0400

    basic profile signup done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503145916) do
+ActiveRecord::Schema.define(version: 20160504123523) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,6 +32,10 @@ ActiveRecord::Schema.define(version: 20160503145916) do
     t.float    "longitude"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
